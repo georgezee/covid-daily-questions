9ed69369ce63ad76eecaf1abb2d8babe180c9c10
George Ziady <george@springfisher.com>
2020-05-29 22:49:01 +0200

    Randomize relative position of answer buttons.

--- src/components/QuestionDisplay.js
+++ src/components/QuestionDisplay.js
@@ -6,6 +6,14 @@ import NextButton from './NextButton';
 
 class QuestionDisplay extends React.Component {
 
+  // We want to return a pseudo-random number based on a certain seed.
+  // stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
+  seededRandom(seed) {
+    seed++;
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+  }
+
   render() {
     return (
       <div>
@@ -16,11 +24,38 @@ class QuestionDisplay extends React.Component {
         </header>
         <div className="App-main">
         {this.props.questions.map((alternatives, i) => {
+          const acceptAnswer = <AnswerButton
+            answerState={this.props.answers[i]}
+            title={alternatives[0].accepted}
+            questionID={i}
+            answerType={'accept'}
+            handleAnswer={this.props.handleAnswer}
+          />;
+
+          const cautionAnswer = <AnswerButton
+            answerState={this.props.answers[i]}
+            title={alternatives[0].cautioned}
+            questionID={i}
+            answerType={'caution'}
+            handleAnswer={this.props.handleAnswer}
+          />;
+
+          let firstAnswer = acceptAnswer;
+          let secondAnswer = cautionAnswer;
+
+          // The seed changes every day, ensuring the button placement is not always consistent.
+          let seed = i + new Date().getDate();
+          // We have a 50% chance of switching the answers around.
+          if (this.seededRandom(seed) > 0.5) {
+            firstAnswer = cautionAnswer;
+            secondAnswer = acceptAnswer;
+          }
+
           return (
             <div key={i} className="App-question">
               <Question title={alternatives[0].question} />
-              <AnswerButton answerState={this.props.answers[i]} title={alternatives[0].accepted} questionID={i} answerType={'accept'} handleAnswer={this.props.handleAnswer}/>
-              <AnswerButton answerState={this.props.answers[i]} title={alternatives[0].cautioned} questionID={i} answerType={'caution'} handleAnswer={this.props.handleAnswer}/>
+              {firstAnswer}
+              {secondAnswer}
             </div>
           )
         })}
