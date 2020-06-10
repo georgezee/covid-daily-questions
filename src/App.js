db839582a67b36ff41608f5cb8470e2c3dfeef1f
George Ziady <george@springfisher.com>
2020-06-10 23:47:59 +0200

    Switch to SCSS file (and add SASS dependency).

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 import QuestionDisplay from './components/QuestionDisplay';
 import StatusDisplay from './components/StatusDisplay';
