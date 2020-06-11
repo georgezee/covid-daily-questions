d5af4760f48aec3e4b895a5ae5348868e961e274
George Ziady <george@springfisher.com>
2020-06-11 23:28:12 +0200

    Update about page text.

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,10 +5,13 @@ const AboutPage = ({visible, handleClick}) => {
 
   return (
     <div className="App-about-page" style={{display: visible ? 'block' : 'none' }}>
-      Are you feeling fine?
+      <h2>The Daily Covid Question:</h2>
+      <h3>Are you feeling fine?</h3>
       <hr/>
       <p>
-        Answer these Covid questions daily - and show at the entrance to your school, work or other public place (from a safe distance!).<br/>
+        Answer these Covid questions daily - and show at the entrance to your school, work or other public place (from a safe distance).
+      </p>
+      <p>
         Show that you've considered your health and the safety of others by showing the status of your questions.
       </p>
 
@@ -17,8 +20,13 @@ const AboutPage = ({visible, handleClick}) => {
       </p>
 
       <p>
-        Background:<br/>
+        <strong>Background:</strong><br/>
         This is an open initiative that began at the <a href="https://codebridge.org.za/">Cape Town Codebridge</a> community evenings.<br/>
+      </p>
+
+      <p>
+        <strong>Privacy:</strong><br/>
+        We don't collect any information on your visit. No external connections are made, and no cookies are stored.<br/>
       </p>
       <br/>
       <a href="https://github.com/georgezee/covid-daily-questions">&lt;View the source on Github/&gt;</a>
